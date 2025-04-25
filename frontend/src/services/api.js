6c08cb60e7525b5afb4df737c4658eb11ea19cb2
not-a-coder <akashxjr10vishwakarma@gmail.com>
2025-04-25 20:56:58 +0530

    added backend domain to frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://movie-recommender-backend-3ze5.onrender.com/api';
 
 export const searchMovies = async (query) => {
   try {
